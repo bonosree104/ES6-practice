caacf60eaf03923c7e65e3db3a8502f4ba0fc7c0
bonosree104 <bonosreeela104@gmail.com>
2021-04-10 06:36:49 +0600

    letconst commit

---
+++ letconst.js
@@ -0,0 +1,18 @@
+const hubby = "elias kanchon";
+//hubby="toto";
+console.log(hubby);
+
+let patientName="rohima";    //const patientName="rohima";(not give result for const)
+patientName= "kolima";
+console.log(patientName);
+
+const numbers=[23,44,77];
+numbers[2]=99;
+numbers.push(56);
+console.log(numbers);
+
+let sum = 0;
+for(var i =0;  i<10;  i++ ) {
+    sum = sum + i;
+}
+console.log(i);
