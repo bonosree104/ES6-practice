7d35f24e8cc53b6713ba3fc1abfacfbf0eee0cf8
bonosree104 <bonosreeela104@gmail.com>
2021-04-10 10:49:32 +0600

    template javascript

---
+++ template.js
@@ -0,0 +1,20 @@
+const firstName = "Bonosree";
+const lastName ="Roy";
+const fullName = firstName + " " + lastName +  " is a good girl ";
+console.log(fullName);
+
+//NEW Javascript //
+const fullName2=`${firstName} ${lastName} is a good girl in her class. `;
+console.log(fullName2);
+
+const multiLine =` I am a person who love coding.
+I want to become a world class programmer
+this year is my programming learnig year.`
+console.log(multiLine);
+
+
+// the old way javascript//
+const multiline2= "i am web developer\n"
++"i am very sincere about my work\n"
++"i love to problem solving"
+console.log(multiline2);
