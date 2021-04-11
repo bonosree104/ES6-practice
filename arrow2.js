0f3459d2947604350ff9c8625fe09248b77b27fc
bonosree104 <bonosreeela104@gmail.com>
2021-04-11 11:08:39 +0600

    arrow commited

---
+++ arrow2.js
@@ -0,0 +1,35 @@
+//function doubleIt(num){
+  // return num*2;
+//}
+//const result = doubleIt(5);
+//console.log(result);
+
+//const doubleIt = function myFun(num){
+  //  return num*2
+//}
+//const result = doubleIt(15);
+//console.log(result);
+
+//const doubleIt = num => num*2;
+//const result = doubleIt(15);
+//console.log(result);//
+
+
+const add = (x,y) => x+y;
+const give5 = () =>5;
+const result = add(15,20);
+const result2 = give5();
+console.log(result);
+console.log(result2);
+
+
+const doMath = (x,y) => {
+    const sum = x+y;
+    const diff = x-y;
+    const result3 = sum*diff;
+    return result3;
+}
+const result3= doMath(20,10);
+console.log(result3);
+
+
