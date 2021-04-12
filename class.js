b1ce9a1deafb8a7c1edba0b403335e947257203e
bonosree104 <bonosreeela104@gmail.com>
2021-04-12 06:12:14 +0600

    class comitted

---
+++ class.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(sId, sName){
+        this.id =sId;
+        this.name = sName;
+
+    }
+}
+const student1 = new Student(12, "Tom");
+const student2 = new Student(22, "mke");
+console.log(student1,student2);
